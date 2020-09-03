8cafda79a06c8308b1a52912809a39c079907fca
MarianVinas <marian-vinas@lambdastudents.com>
2020-09-02 20:58:39 -0700

    Completed initial packaging in class

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Lambdata-dspt7-mvinas", # Replace with your own username
+    version="0.0.1",
+    author="Marian Vinas",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marianvinas/Lambdata-DSPT7.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
