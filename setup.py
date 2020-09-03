b695f7dea6ea9f8e6e674206a5419df46784a6f1
MarianVinas <marian-vinas@lambdastudents.com>
2020-09-02 21:02:16 -0700

    fixed setup.py url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/marianvinas/Lambdata-DSPT7.git",
+    url="https://github.com/marianvinas/Lambdata-DSPT7",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
